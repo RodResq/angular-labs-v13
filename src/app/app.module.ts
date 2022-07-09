6be2149ab74db73912dc42523a3483c3b9480cea
RodResq <rodrigoresque@yahoo.com.br>
2022-07-09 17:13:07 -0300

    updt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,19 @@
-import { ServerComponent } from './server/server.component';
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ServerComponent } from './server/server.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
 
 import { AppComponent } from './app.component';
 import { ServersComponent } from './servers/servers.component';
+import { SuccesAlertComponent } from './succes-alert/succes-alert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    SuccesAlertComponent,
+    WarningAlertComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/succes-alert/succes-alert.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-success-alert',
+  templateUrl: './succes-alert.component.html',
+  styleUrls: ['./succes-alert.component.css']
+})
+export class SuccesAlertComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
